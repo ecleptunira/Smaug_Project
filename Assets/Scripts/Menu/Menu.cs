90b825e7383bbfafed037ce6db1fc387f265086e
camila-94 <114260291+camila-94@users.noreply.github.com>
2022-11-08 18:48:18 -0300

    feat: adiciona o painel de credits

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
-{
+{   
+    public GameObject credits;
+
+
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);        
+    }
+
+    public void Credits()
+    {
+        credits.SetActive(true);
+        
+    }
+
+    public void CloseCredits()
+    {
+        credits.SetActive(false);
     }
 }
