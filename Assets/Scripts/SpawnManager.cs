264c0c850ba42a359411e39d773e85fa9471688c
camila-madureira <121184472+ca-madureira@users.noreply.github.com>
2023-04-20 01:36:33 -0300

    update lifeEnemy and countInStockEnemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,7 +11,7 @@ public class SpawnManager : MonoBehaviour
     GameObject player;
     
     int index;
-    int stockEnem = 35;
+    public int stockEnem;
     bool boss = true;
     float delay = 25;
 
