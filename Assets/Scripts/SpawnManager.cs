4393577b7cf0e6ce01e67debfdc0993bcc9cf0da
camila-madureira <121184472+ca-madureira@users.noreply.github.com>
2023-04-20 01:22:13 -0300

    Squirrel and Player Attack Fixes

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,7 +11,7 @@ public class SpawnManager : MonoBehaviour
     GameObject player;
     
     int index;
-    int stockEnem = 2;
+    int stockEnem = 35;
     bool boss = true;
     float delay = 25;
 
