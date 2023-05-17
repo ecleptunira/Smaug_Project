6b2fb87e660dfba93f1a332ff8f313214e907aeb
camila-madureira <121184472+ca-madureira@users.noreply.github.com>
2023-05-17 11:07:27 -0300

    fixes mute/desmute and your icon

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sound : MonoBehaviour
 {
     public static Sound  instance; 
+    private bool audioOn = true;
     public AudioSource soundPlayer, soundEnemy, soundAttack;
+    public Sprite[] clickedSprite;
+    private Image buttonImage;
     // Start is called before the first frame update
     void Awake(){
         instance = this;
     }
     void Start()
     {
-        
+        buttonImage = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void VolumeGame(){
+        audioOn = !audioOn;
+        if(audioOn){
+            AudioListener.volume = 1;
+            buttonImage.sprite = clickedSprite[0];
+        } else{
+            AudioListener.volume = 0;
+            buttonImage.sprite = clickedSprite[1];
+        }
     }
 }
